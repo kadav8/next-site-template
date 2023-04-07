d5c83632eacb5f9d5f9277ab0f8f4c594a8e5a6d
kadav8 <kadav8@gmail.com>
2023-04-07 09:40:18 +0200

    design

--- app/[lang]/menubar.js
+++ app/[lang]/menubar.js
@@ -6,13 +6,19 @@ export default async function Menubar(props) {
    const t = await dictionary(props.lang)
 
    return (
-      <div className="flex bg-gray-100">
-         <div className="flex flex-1 ml-5">
+      <div className="flex bg-indigo-600 text-white justify-center">
+         <div className="w-20 text-start py-4 font-extrabold">
+            Template
+         </div>
+         <div className="flex px-72">
             <MenuLink href={`/${props.lang}`} name={t['navigation'].home_title}/>
             <MenuLink href={`/${props.lang}/${t['navigation'].about_url}`} name={t['navigation'].about_title}/>
+            <MenuLink href={`/${props.lang}/${t['navigation'].about_url}`} name={t['navigation'].about_title}/>
+            <MenuLink href={`/${props.lang}/${t['navigation'].about_url}`} name={t['navigation'].about_title}/>
+            <MenuLink href={`/${props.lang}/${t['navigation'].about_url}`} name={t['navigation'].about_title}/>
          </div>
-         <div className="mr-5 p-3">
-            <LanguageSelector />
+         <div className="w-20 text-end py-4">
+            <span className="border border-white rounded-lg py-1 px-3 text-xs"><LanguageSelector/></span>
          </div>
       </div>
    )
@@ -20,8 +26,8 @@ export default async function Menubar(props) {
 
 function MenuLink(props) {
    return (
-      <div className="p-3">
-         <Link href={props.href} className="text-md">{props.name}</Link>
+      <div className="py-4 px-5">
+         <Link href={props.href} className="text-sm font-semibold hover:underline hover:cursor-pointer">{props.name}</Link>
       </div>
    )
  }
--- app/[lang]/page.js
+++ app/[lang]/page.js
@@ -4,8 +4,38 @@ export default async function Page({ params: { lang } }) {
   const t = await dictionary(lang)
   
   return (
-    <div className="flex flex-col p-8">
-      <div className="text-2xl mb-12">{t['hello-message']}</div>
+    <div className="flex flex-col">
+      <div className="flex flex-col h-[36rem] text-white items-center bg-indigo-600 bg-cover	bg-[url('/front.svg')]">
+        <span className="pt-24 pb-8 text-5xl font-extrabold uppercase">
+          {t['hello-message']}
+        </span>
+        <span className="w-[48rem] text-center text-lg tracking-wider leading-8">
+          Vivamus a malesuada libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sollicitudin odio nec consequat feugiat. 
+          Maecenas pulvinar porttitor mauris ut sollicitudin. Etiam sit amet mi sed quam sagittis eleifend.
+        </span>
+        <div className="pt-16">
+          <button class="mx-4 py-3 px-5 rounded-full bg-white text-black hover:shadow-2xl hover:scale-[1.015]">{t['button1']}</button>
+          <button class="mx-4 py-3 px-5 rounded-full bg-black text-white hover:shadow-2xl hover:scale-[1.015]">{t['button2']}</button>
+        </div>
+      </div>
+
+      <div className="flex flex-col h-[32rem] p-32">
+        <span className="w-96 font-semibold text-2xl">
+          Vivamus a malesuada libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </span>
+      </div>
+
+      <div className="flex flex-col h-[32rem] p-32 bg-stone-50">
+        <span className="w-96 font-semibold text-2xl">
+          Vivamus a malesuada libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </span>
+      </div>
+
+      <div className="flex flex-col h-[32rem] p-32">
+        <span className="w-96 font-semibold text-2xl">
+          Vivamus a malesuada libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </span>
+      </div>
     </div>
   )
 }
