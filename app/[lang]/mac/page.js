79e65c05eefa3cc68b9f306256d0937fa8aca3c5
kadav8 <kadav8@gmail.com>
2023-04-10 18:18:42 +0200

    responsive mac page

--- app/[lang]/mac/page.js
+++ app/[lang]/mac/page.js
@@ -7,7 +7,7 @@ export const metadata = {
 export default async function MacPage() {  
   return (
     <div className="flex flex-col items-center bg-stone-100">
-        <div className="flex flex-col text-[2rem] sm:text-[3.5rem] lg:text-[6rem] font-semibold leading-none my-60">
+        <div className="flex flex-col text-[3rem] sm:text-[4.5rem] lg:text-[6rem] font-semibold leading-none my-44">
           <TickSvg text="Easy"></TickSvg>
           <TickSvg text="Powerful"></TickSvg>
           <TickSvg text="Amazing"></TickSvg>
@@ -16,10 +16,10 @@ export default async function MacPage() {
           </span>
         </div>
 
-        <div className="flex text-white w-4/5">
-          <div className="flex flex-col bg-black p-16 w-2/3 mx-3 rounded-3xl">
+        <div className="flex flex-col md:flex-row w-11/12 xl:w-4/5 text-white">
+          <div className="flex flex-col bg-black p-16 lg:p-20 md:w-1/2 xl:w-2/3 md:mx-3 mb-8 rounded-3xl">
             <span className="text-6xl font-extrabold">Intuitive by design.</span>
-            <span className="text-lg my-8 w-2/3">Mac is designed to be easy to learn and use — so you can do more than you ever imagined.</span>
+            <span className="text-lg my-8 xl:w-3/4">Mac is designed to be easy to learn and use — so you can do more than you ever imagined.</span>
             <div className="flex justify-center">           
               <Image
                 src="/macbook.jpeg"
@@ -30,27 +30,27 @@ export default async function MacPage() {
               />
             </div>
           </div>
-          <div className="flex flex-col bg-black p-16 w-1/3 mx-3 rounded-3xl justify-center">
+          <div className="flex flex-col bg-black p-16 md:w-1/2 xl:w-1/3 md:mx-3 mb-8 rounded-3xl justify-center">
             <span className="text-6xl font-extrabold">If you love iPhone, <span className="text-lime-400">YOU WILL LOVE MAC.</span></span>
             <span className="text-lg mt-8">You will feel right at home on Mac, with many of the same features and apps as iPhone.</span>
           </div>
         </div>
 
-        <div className="flex w-4/5 my-8">
-          <div className="flex flex-col bg-white text-black text-5xl font-extrabold p-24 w-1/2 mx-3 rounded-3xl justify-center">
+        <div className="flex flex-col md:flex-row w-11/12 xl:w-4/5">
+          <div className="flex flex-col bg-white text-black text-4xl xl:text-5xl font-extrabold p-16 xl:p-20 w-full md:w-1/2 md:mx-3 mb-8 rounded-3xl justify-center">
             <span className="py-5">All your things.</span>
             <span className="py-5">All in iCloud.</span>
             <div className="py-5">
               <span className="italic bg-lime-400 p-2">ALL IN SYNC.</span>
             </div>
           </div>
-          <div className="flex flex-col bg-black text-white p-16 w-1/2 mx-3 rounded-3xl justify-center">
-            <span className="text-6xl font-extrabold p-24">Watch<br />the whole <br />season on a <br /><span className="text-lime-400">SINGLE<br /> CHARGE.</span></span>
+          <div className="flex flex-col bg-black text-white p-16 xl:p-20 w-full md:w-1/2 md:mx-3 mb-8 rounded-3xl justify-center">
+            <span className="text-4xl xl:text-6xl font-extrabold">Watch<br/>the whole <br/> season on a <br/><span className="text-lime-400">SINGLE <br/> CHARGE.</span></span>
           </div>
         </div>
 
-        <div className="flex w-4/5 bg-white rounded-3xl mb-24">
-          <div className="flex flex-col  text-black p-24 pl-0 w-2/3 justify-center">
+        <div className="flex w-11/12 xl:w-4/5 bg-white rounded-3xl mb-24">
+          <div className="flex flex-col text-black p-12 pr-2 pl-0 lg:p-24 lg:pl-0 w-2/3 justify-center">
             <Image
                 src="/macbook2.jpg"
                 alt="Macbook"
@@ -59,8 +59,8 @@ export default async function MacPage() {
             />
           </div>
           <div className="flex flex-col text-black w-1/3 justify-center">
-            <span className="text-5xl font-extrabold">Sprint through anything with</span>
-            <span className="text-6xl font-extrabold text-lime-400 italic py-5">APPLE <br /> SILICON.</span>
+            <span className="text-2xl lg:text-5xl font-extrabold">Sprint through anything with</span>
+            <span className="text-4xl lg:text-6xl font-extrabold text-lime-400 italic py-2">APPLE <br /> SILICON.</span>
           </div>
         </div>
     </div>
