6ae0d35f71006720f8406c84a6e93b2dd3e50c14
kadav8 <kadav8@gmail.com>
2023-04-11 12:57:05 +0200

    home page

--- app/[lang]/page.js
+++ app/[lang]/page.js
@@ -1,11 +1,12 @@
+import Image from "next/image";
 import { dictionary } from '../../dictionary'
 
 export default async function Page({ params: { lang } }) {
   const t = await dictionary(lang)
   
   return (
-    <div className="flex flex-col">
-      <div className="flex flex-col h-[34rem] text-white items-center bg-indigo-600 bg-cover	bg-[url('/front.svg')]">
+    <div className="flex flex-col items-center">
+      <div className="flex flex-col w-full h-[34rem] text-white items-center bg-indigo-600 bg-cover	bg-[url('/front.svg')]">
         <span className="pt-24 pb-8 text-5xl font-extrabold uppercase">
           {t['hello-message']}
         </span>
@@ -19,22 +20,43 @@ export default async function Page({ params: { lang } }) {
         </div>
       </div>
 
-      <div className="flex flex-col h-[32rem] p-16 md:p-32">
-        <span className="w-4/5 lg:w-96 font-semibold text-2xl">
-          Vivamus a malesuada libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        </span>
+      <div className="flex flex-row items-center bing-bg rounded-2xl mt-24">
+        <div className="flex justify-center items-center w-[10rem] xl:w-[16rem]">
+          <div className="relative w-[8rem] h-[8rem]"><Image src="/bing.png" alt="Bing" fill/></div>
+        </div>
+        <div className="flex flex-col pl-6 pr-8 py-20 w-[22rem] lg:w-[32rem] xl:w-[40rem]">
+          <span className="text-2xl xl:text-4xl mb-8">Bemutatjuk az új Binget. Az AI-alapú másodpilóta a weben.</span>
+          <span className="text-md xl:text-lg">Az új Bing esetében a válaszok csak a kezdetet jelentik. Tegyen fel valódi kérdéseket. Finomítson a csevegésben. Átfogó válaszokat kaphat. Alakítsa át az ötleteket vázlatokká. Keresse meg a Microsoft Edge-be beépítve, amely az új Bing legjobb böngészője.</span>
+        </div>
+        <div className="hidden md:flex flex-col w-[15rem] xl:w-[18rem]">
+          <button className="p-3 w-[12rem] xl:w-[15rem] rounded-md bg-black text-white">Csatlakozás a várólistához</button>
+        </div>
       </div>
 
-      <div className="flex flex-col h-[32rem] p-16 md:p-32 bg-stone-50">
-        <span className="w-4/5 lg:w-96 font-semibold text-2xl">
-          Vivamus a malesuada libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        </span>
+      <div className="flex flex-col lg:flex-row my-24">
+        <div className="flex flex-col pt-2 mb-8 lg:mb-0 w-[25rem] xl:w-[32rem]">
+          <span className="text-2xl xl:text-4xl">Érj el nagyobb teljesítményt</span>
+          <span className="text-md xl:text-lg py-4 lg:py-16 lg:pr-6">A Microsoft Edge ugyanarra a technológiára épül, mint a Chrome, azonban további beépített funkciókkal rendelkezik – mint például az indítási rásegítés és az alvó lapok –, melyek a Windowszal legjobban való működésre optimalizált, világszínvonalú teljesítménnyel és sebességgel teszik jobbá a böngészési élményt.</span>
+          <button className="p-3 w-[16rem] rounded-md bg-black text-white">További információk</button>
+        </div>
+        <div className="flex justify-end w-[32rem] xl:w-[40rem]">
+          <div className="relative w-[35rem] h-[25rem]">
+            <Image src="/laptop.jpg" alt="Laptop" fill className="rounded-2xl"/>
+          </div>
+        </div>
       </div>
 
-      <div className="flex flex-col h-[32rem] p-16 md:p-32">
-        <span className="w-4/5 lg:w-96 font-semibold text-2xl">
-          Vivamus a malesuada libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        </span>
+      <div className="flex flex-row items-center bg-stone-100 rounded-2xl mb-24">
+        <div className="flex justify-center items-center w-[10rem] xl:w-[16rem]">
+          <div className="relative w-[8rem] h-[8rem]"><Image src="/secure-icon.png" alt="Secure" fill/></div>
+        </div>
+        <div className="flex flex-col pl-6 pr-8 py-20 w-[22rem] lg:w-[32rem] xl:w-[40rem]">
+          <span className="text-2xl xl:text-4xl mb-8">Maradj biztonságban online.</span>
+          <span className="text-md xl:text-lg">A Microsoft Edge beépített biztonsági és adatvédelmi funkciókkal rendelkezik, mint például a Microsoft Defender SmartScreen, a Jelszófigyelő, az InPrivate-keresés és a Gyermek mód, melyek segítenek megvédeni téged és szeretteidet, és megőrizni az online biztonságotokat.</span>
+        </div>
+        <div className="hidden md:flex flex-col w-[15rem] xl:w-[18rem]">
+          <button className="p-3 w-[12rem] xl:w-[15rem] rounded-md bg-black text-white">További információk</button>
+        </div>
       </div>
     </div>
   )
