5c99ff69c591a7cb80d7325bb09942fe52d51b9a
kadav8 <kadav8@gmail.com>
2023-04-17 11:28:06 +0200

    animation fix

--- components/anim-div.js
+++ components/anim-div.js
@@ -1,9 +1,23 @@
 'use client'
 
+import React, { useState, useEffect } from 'react';
 import { useInView } from "react-intersection-observer";
 
 export default function AnimDiv({ children }) {
+  const [noscript, setNoscript] = useState(true);
   const { ref, inView } = useInView({ threshold: 0.5, triggerOnce: true });
+
+  useEffect(() => {
+    setNoscript(false)
+  }, []);
+
+  if(noscript) {
+    return (
+      <div ref={ref} className="flex flex-col items-center w-full delayedFadeInUp">
+       {children} 
+      </div>
+    );
+  }
 
   return (
     <div ref={ref} className={`flex flex-col items-center w-full ${inView ? 'visible fadeInUp2' : 'invisible'}`}>
