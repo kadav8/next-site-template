47e07b5bb3e0d9a2d3c8735c39763eec1b1f00ed
kadav8 <kadav8@gmail.com>
2023-05-19 14:24:52 +0200

    API_BASE_URL

--- app/[lang]/[column]/[slug]/page.tsx
+++ app/[lang]/[column]/[slug]/page.tsx
@@ -2,19 +2,20 @@ import { getContent } from '@/services/articleContentService';
 import Link from 'next/link';
 import notFound from './not-found';
 
+const API_BASE_URL = process.env.PUBLIC_API_KEY || '';
 const PUBLIC_API_KEY = process.env.PUBLIC_API_KEY || '';
-
-export const metadata = {
-	title: 'Site Template - Articles',
-};
 
 export const revalidate = 'force-cache';
 export const dynamic = 'force-static';
 export const dynamicParams = true;
 
+export const metadata = {
+	title: 'Site Template - Articles',
+};
+
 export default async function ArticlePage({ params }) {
 	const { lang, slug } = params;
-	const res = await fetch('https://codeside-cms-eight.vercel.app/api/public/articles/' + slug, { next: { revalidate: 43200 }, headers: { "Api-Key": PUBLIC_API_KEY } });
+	const res = await fetch(API_BASE_URL + '/articles/' + slug, { next: { revalidate: 43200 }, headers: { "Api-Key": PUBLIC_API_KEY } });
 	const article = await res.json();
 	if (!article) notFound();
 	const articleContent = article.content;
